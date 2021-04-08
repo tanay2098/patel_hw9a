53d8584acf0f202e639b243b8c4f06b1e9458aa8
Tanay Patel <tanay.patel@student.csulb.edu>
2021-04-08 11:54:49 -0700

    ex3_commit2

--- ex3/index.js
+++ ex3/index.js
@@ -1,4 +1,4 @@
 const moment=require("moment");
 
-console.log(moment().format("dddd,MMMM Do YYYY,h:mm:ss a"));
+console.log(moment().format("dddd,MMMM Do YYYY"));
 console.log(moment("1976-11-26").fromNow());
